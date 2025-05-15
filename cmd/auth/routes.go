61b47e27f728d24c2e08299de36a14d487851fe9
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2025-05-15 03:03:51 +0200

    update: new feature or fix

--- cmd/auth/routes.go
+++ cmd/auth/routes.go
@@ -1,5 +1,7 @@
 package auth
 
+import "github.com/gofiber/fiber/v2"
+
 func (s *ServerState) setupRoutes() {
 	authGroup := s.fiberApp.Group("/auth")
 	authGroup.Post("/sign-in", s.handler.SignIn)
@@ -12,4 +14,11 @@ func (s *ServerState) setupRoutes() {
 	authGroup.Post("/reset-password/confirm", s.handler.ForgotPasswordConfirm)
 	authGroup.Post("/me", s.handler.Me)
 	authGroup.Post("/kyc-verify", s.handler.KYCVerify)
+	authGroup.Get("/health", func(ctx *fiber.Ctx) error {
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "OK from auth service",
+			"data":    "I am on live ",
+		})
+	})
 }
